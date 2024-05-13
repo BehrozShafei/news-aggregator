83cca7165f9606f19002130be822b33e754b9713
behroozshafei.official <behroozshafei.official@gmail.com>
2024-05-13 20:05:00 +0330

    add main style

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Blog from './views/Blog';
-import MyFeed from './views/MyFeed';
-import LandingPageLayout from './layouts/LandingPageLayout';
-import MyFeedLayout from './layouts/MyFeedLayout';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Blog from "./views/Blog";
+import MyFeed from "./views/MyFeed";
+import LandingPageLayout from "./layouts/LandingPageLayout";
+import MyFeedLayout from "./layouts/MyFeedLayout";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <LandingPageLayout>
-              <Blog />
-            </LandingPageLayout>
-          }
-        />
-        <Route
-          path="/:id"
-          element={
-            <LandingPageLayout>
-              <Blog />
-            </LandingPageLayout>
-          }
-        />
-        <Route
-          path="/MyFeed"
-          element={
-            <MyFeedLayout>
-              <Route path="/MyFeed" element={<MyFeed />} />
-              <Route path="/MyFeed/:id" element={<MyFeed />} />
-            </MyFeedLayout>
-          }
-        />
-      </Routes>
-    </Router>
+    <div className="App">
+      <div className="main">
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <LandingPageLayout>
+                  <Blog />
+                </LandingPageLayout>
+              }
+            />
+            <Route
+              path="/:id"
+              element={
+                <LandingPageLayout>
+                  <Blog />
+                </LandingPageLayout>
+              }
+            />
+            <Route
+              path="/MyFeed"
+              element={
+                <MyFeedLayout>
+                  <Route path="/MyFeed" element={<MyFeed />} />
+                  <Route path="/MyFeed/:id" element={<MyFeed />} />
+                </MyFeedLayout>
+              }
+            />
+          </Routes>
+        </Router>
+      </div>
+    </div>
   );
 };
 
